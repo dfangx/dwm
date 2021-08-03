28efc514be4f0158afcb985aec11363651d4bc10
cyrusng <cyrus.ng@protonmail.com>
2021-08-02 22:57:11 -0400

    nord theme

---
+++ colors/nord.h
@@ -0,0 +1,34 @@
+static char normfgcolor[] = "#81a1c1";
+static char normbgcolor[] = "#2E3440";
+static char normbordercolor[] = "#4C566A";
+static char normfloatcolor[]             = "#3A3A3A";
+
+static char selfgcolor[] = "#88c0d0";
+static char selbgcolor[] = "#434c5e";
+static char selbordercolor[] = "#8FBCBB";
+static char selfloatcolor[]              = "#005577";
+
+static char titlenormfgcolor[] = "#81a1c1";
+static char titlenormbgcolor[] = "#2E3440";
+static char titlenormbordercolor[] = "#4C566A";
+static char titlenormfloatcolor[] = "#3A3A3A";
+
+static char titleselfgcolor[] = "#88c0d0";
+static char titleselbgcolor[] = "#434c5e";
+static char titleselbordercolor[] = "#8FBCBB";
+static char titleselfloatcolor[] = "#005577";
+
+static char tagsnormfgcolor[] = "#81a1c1";
+static char tagsnormbgcolor[] = "#2E3440";
+static char tagsnormbordercolor[] = "#4C566A";
+static char tagsnormfloatcolor[] = "#3A3A3A";
+
+static char tagsselfgcolor[] = "#88c0d0";
+static char tagsselbgcolor[] = "#434c5e";
+static char tagsselbordercolor[] = "#8FBCBB";
+static char tagsselfloatcolor[] = "#005577";
+
+static char urgfgcolor[] = "#8FBCBB";
+static char urgbgcolor[] = "#88C0D0";
+static char urgbordercolor[] = "#88C0D0";
+static char urgfloatcolor[] = "#db8fd9";
